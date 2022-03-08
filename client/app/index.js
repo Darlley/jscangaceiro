57748c79e25c7b674e716b30e27f236c801c4fab
Darlley <darlleybrito@gmail.com>
2022-03-08 16:10:47 -0400

    referencias querySelector e evento de submit

---
+++ client/app/index.js
@@ -0,0 +1,18 @@
+let campos = [
+    document.querySelector('#data'),
+    document.querySelector('#quantidade'),
+    document.querySelector('#valor'),
+];
+console.log(campos)
+
+
+// let tbody = document.querySelector('[data-js="tbody"]');
+// let btn_apagar = document.querySelector('[data-js="btn-apagar"]');
+// let btn_importar = document.querySelector('[data-js="btn-importar"]');
+
+
+// let btn_incluir = document.querySelector('[data-js="btn-incluir"]');
+let $formulario = document.querySelector('.form')
+$formulario.addEventListener('submit', event => {
+    alert('oi')
+})
